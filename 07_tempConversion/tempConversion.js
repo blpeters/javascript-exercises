949c9cffed55deec6a59184263288c0739ce1ea4
blpeters <brett.peters@utexas.edu>
2021-11-15 21:25:34 -0600

    tempConversion solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,19 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let celsius = Math.abs((f - 32) * (5 / 9));
+  let celsiusRounded = Math.round((celsius * 10)) / 10;
+  if(((f - 32) * (5 / 9)) < 0) {
+    return (-celsiusRounded);
+  }
+  return celsiusRounded;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let fahrenheit = Math.abs(c * (9 / 5) + 32);
+  let fahrenheitRound = Math.round((fahrenheit * 10)) /10;
+  if (c * (9 / 5) + 32 < 0) {
+    return (-fahrenheitRound);
+  }
+  return fahrenheitRound;
 };
 
 // Do not edit below this line
