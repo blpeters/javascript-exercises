6f1c00d288226d7e2b4ee9e683acd0cdda394e6a
blpeters <brett.peters@utexas.edu>
2022-09-07 09:41:13 -0500

    calculator initial solutions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  return numbers.reduce((total, number) => {
+    return total + number;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  return numbers.reduce((total, number) => {
+    return total * number;
+  });
+  //The reason you don't need the default value of 0 after the reduce function lie we did for the sum function (above) is that giving the initial value of total of 0 causes a multiple of zero, which is always zero. We can either put 1 or leave it blank (undefined until the next loop)
 };
 
-const power = function() {
-	
+const power = function (number, powerNumber) {
+  return number ** powerNumber;
 };
 
-const factorial = function() {
-	
+//This factorial method doesn't seem very clean but it works. Compare to the TOP solution for code simplicity/elegance.
+const factorial = function (number) {
+  let facTotal = 1;
+  while (number >= 1) {
+    facTotal = facTotal * number;
+    number--;
+  }
+  return facTotal;
 };
 
 // Do not edit below this line
@@ -29,5 +40,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
