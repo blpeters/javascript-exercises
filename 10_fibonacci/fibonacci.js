6b415f9c28ec6234c5a34d77a6dd2f01d28a6ba4
blpeters <brett.peters@utexas.edu>
2022-09-07 11:32:35 -0500

    fibonacci initial solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (number) {
+  //1,1,2,3,5,8,13,21...
+  number = parseInt(number);
+  if (number >= 0) {
+    const fib = [1, 1];
+    let n = 0;
+    for (let i = 1; i < number; i++) {
+      n = fib[i] + fib[i - 1];
+      fib.push(n);
+    }
 
+    return fib[fib.length - 2];
+  } else return "OOPS";
 };
 
 // Do not edit below this line
