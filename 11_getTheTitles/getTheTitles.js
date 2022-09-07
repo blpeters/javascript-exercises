1cee1b50aa6e560afdace849dcc61644ced1f46e
blpeters <brett.peters@utexas.edu>
2022-09-07 14:12:39 -0500

    findTheOldest .sort solution

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (obj) {
+  const titles = obj.map((item) => item.title);
+  return titles;
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  //Check for year of death. If none, use current year.
+  for (let person in people) {
+    if (!people[person]["yearOfDeath"]) {
+      let currentYear = new Date().getFullYear();
+      people[person]["yearOfDeath"] = `${currentYear}`;
+    }
+  }
 
+  //Used sort, but a better way would be to use reduce to systematically step through comparing each age, and carrying through the oldest.
+  const peopleSortedByAge = people.sort((a, b) =>
+    a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth ? 1 : -1
+  );
+  return peopleSortedByAge[peopleSortedByAge.length - 1];
 };
 
 // Do not edit below this line
