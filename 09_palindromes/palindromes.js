c5f180b7b33baf313bcc72813f4c46ccfb46c46a
blpeters <brett.peters@utexas.edu>
2022-09-07 11:02:03 -0500

    palindrome initial solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (phrase) {
+  let phraseEdited = phrase
+    .toLowerCase()
+    .match(/[^_\W]+/g)
+    .join("");
+  console.log(phraseEdited);
 
+  const phraseArr = phraseEdited.split("");
+  console.log(phraseArr);
+
+  //Create another string or array in reverse. Must use slice to create a copy before reversing. Otherwise both arrays are reversed.
+  const reversedArr = phraseArr.slice().reverse();
+
+  //Compare the two strings/arrays and return result to complete function.
+
+  return phraseArr.toString() === reversedArr.toString();
 };
 
 // Do not edit below this line
